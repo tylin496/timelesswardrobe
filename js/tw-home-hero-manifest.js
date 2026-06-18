336b1cda3f605bca713d19e15dededa57954f792
TY Lin <tylin496@gmail.com>
2026-06-19 01:58:37 +0800

    Fix hero manifest: update to actual files in images/heroes/

--- js/tw-home-hero-manifest.js
+++ js/tw-home-hero-manifest.js
@@ -1,9 +1,10 @@
 window.TW_HOME_HERO_IMAGES = [
-  "images/heroes/0209_country_selector_bg.jpg",
+  "images/heroes/0303_pl_hub_c07_img.jpg",
+  "images/heroes/0512_mlp_c02a_img.jpg",
   "images/heroes/Designer Mens Clothing Luxury Menswear Ralph Lauren UK.mp4",
-  "images/heroes/hero-country-classics.png",
-  "images/heroes/hero-editorial-01.png",
-  "images/heroes/hero-editorial-02.png",
-  "images/heroes/hero.png",
-  "images/heroes/image 08.09.39.png"
+  "images/heroes/Purple Label for Men Ralph Lauren UK.mp4",
+  "images/heroes/greece copy.png",
+  "images/heroes/image 08.09.jpg",
+  "images/heroes/image.jpg",
+  "images/heroes/image的ㄉ.jpg"
 ];
